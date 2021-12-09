0dd8ba43e4464739eb3de01abb8b4b6768079248
Vinyaaas <91446586+Vinyaaas@users.noreply.github.com>
2021-12-09 17:08:59 +0530

    commit

--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -124,7 +125,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'staticfiles')
 ]
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Activate Django-Heroku.
